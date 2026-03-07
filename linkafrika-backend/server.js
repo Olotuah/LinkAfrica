1d00ef63e47cdbcac01fa4d550ac5661b844dc21
Gbenga Olotuah <74839657+Olotuah@users.noreply.github.com>
2026-03-07 16:37:45 +0100

    Update server.js

--- linkafrika-backend/server.js
+++ linkafrika-backend/server.js
@@ -23,7 +23,7 @@ const corsOptions = {
           "https://link-africa.vercel.app", // Your actual Vercel domain
           "https://linkafrica.vercel.app", // Backup domain
           "https://linkafrika.tech",
-          "https://www.linkafrika.tech
+          "https://www.linkafrika.tech"
           process.env.FRONTEND_URL,
         ]
       : [
